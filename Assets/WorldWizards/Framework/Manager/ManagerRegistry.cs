c49d7b164d86c95035f316faf27111f3657a8b0d
jeffpk@worldwizards.net <>
2018-06-08 21:10:13 -0400

    Reorganized and moved WWUTILS to the framework

--- Assets/WorldWizards/Framework/Manager/ManagerRegistry.cs
+++ Assets/WorldWizards/Framework/Manager/ManagerRegistry.cs
@@ -1,8 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using WorldWizards.WWUtils.CSharp.Types;
+
 
 namespace WorldWizards.core.manager
 {
