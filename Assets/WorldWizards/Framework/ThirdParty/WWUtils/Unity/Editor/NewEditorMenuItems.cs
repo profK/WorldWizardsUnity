498bfcff650f773d52dc2f410c894dba1ba024da
jeffpk@worldwizards.net <>
2018-09-21 17:03:12 -0400

    basic walls in on tileset, msot still need textures

---
+++ Assets/WorldWizards/Framework/ThirdParty/WWUtils/Unity/Editor/NewEditorMenuItems.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NewEditorMenuItems{
+    [MenuItem("WWTools/Get Mesh Bounds")]
+    private static void NewMenuOption()
+    {
+        GameObject obj = Selection.activeGameObject;
+        MeshFilter mf = obj.GetComponentInChildren<MeshFilter>();
+        if (mf == null)
+        {
+            Debug.Log("Mesh bounds error: no mesh selected");
+        }
+        else
+        {
+            Bounds mb = mf.sharedMesh.bounds;
+            Debug.Log("Mesh Extents: (" + mb.extents.x + "," + mb.extents.y + "," + mb.extents.z + ")");
+            Debug.Log("Mesh center: ("+mb.center.x+","+mb.center.y+","+mb.center.z+")");
+        }
+    }
+	
+}
