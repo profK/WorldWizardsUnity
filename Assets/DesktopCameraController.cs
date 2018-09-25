5aee8f826dad69d2e4d3a1addbb8a61f9d109906
jeffpk@worldwizards.net <>
2018-09-25 17:45:35 -0400

    second aprt of last checkin

---
+++ Assets/DesktopCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesktopCameraController : MonoBehaviour
+{
+    private Camera cam;
+	// Use this for initialization
+	void Start ()
+	{
+	    cam = GetComponentInChildren<Camera>();
+	    
+	}
+
+    public void DollyZ(float amnt)
+    {
+        gameObject.transform.Translate(new Vector3(0,0,amnt));
+    }
+}
